993b4ef81c211fec6165df966d325ae484180497
kat3005 <kasia.alicja@gmail.com>
2019-08-09 16:00:10 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      articles: [],
+    }
+  },
+  mounted(){
+    fetch('https://content.guardianapis.com/search?q=brexit&format=json&api-key=test')
+    .then(res => res.json())
+    .then(banana => this.articles = banana)
+    
+
+
+
+
   }
 }
 </script>
